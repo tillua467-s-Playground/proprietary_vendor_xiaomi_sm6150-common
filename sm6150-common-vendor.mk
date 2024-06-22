2ca49c248e20721b6af996bb0a72117ad148ef5f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-22 23:54:57 +0200

    sm6150-common: Include libdpps
    W SDM     : DisplayBuiltIn::Init: Failed to load Dpps lib libdpps.so
    
    * [npjohnson]: Pull in missing deps too.
    
    Change-Id: Ic933c1f20b643033e7e89a36d25c4ff8afb70913

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -205,8 +205,10 @@ PRODUCT_PACKAGES += \
     libconfigdb \
     libcpion \
     libdataitems \
+    libdisp-aba \
     libdisplayqos \
     libdpmqmihal \
+    libdpps \
     libdrmfs \
     libdrmtime \
     libdsi_netctrl \
